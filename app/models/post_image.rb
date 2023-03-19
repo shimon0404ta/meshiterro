b1c492820e814f43644189f6caba70ddd35f1e1e
TakumiShimotsuka <124693530+shimon0404ta@users.noreply.github.com>
2023-03-19 18:14:50 +0000

    [Finish] 4-10 投稿機能調整

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,5 +2,13 @@ class PostImage < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  
+　def get_image
+    if image.attached?
+      image
+    else
+      'no_image.jpg'
+    end
+  end
 
 end

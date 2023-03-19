3b826babab0e0b921e62fb11b6e3aaf94e46fe92
TakumiShimotsuka <124693530+shimon0404ta@users.noreply.github.com>
2023-03-19 15:58:30 +0000

    [Finish] 4-9 1:N アソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
+
+  resources :post_images, only: [:new, :index, :show]
+
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about',as: 'about'
+  get "homes/about" => "homes#about", as: "about"
 end
